0eaa5adba04a08ae1f3eb402d87b1e3aaa0742a8
tarlepp <tarmo.leppanen@protacon.com>
2018-09-07 12:04:02 +0300

    clean up

--- DependencyInjection/TestToolsExtension.php
+++ DependencyInjection/TestToolsExtension.php
@@ -16,7 +16,6 @@ class TestToolsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $foo = 'asdf';
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('testtools.xml');
     }
