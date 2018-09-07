17ca26e29dcf1f551be4c49a5e66b011ae9b1699
tarlepp <tarmo.leppanen@protacon.com>
2018-09-07 12:52:14 +0300

    lets try package events

--- Util/Package.php
+++ Util/Package.php
@@ -9,21 +9,25 @@ class Package
 {
     public static function postPackageInstall(PackageEvent $event)
     {
+        file_put_contents('/tmp/temp.txt', 'jee1', FILE_APPEND);
         die('jeee');
     }
 
     public static function postUpdateInstall(PackageEvent $event)
     {
+        file_put_contents('/tmp/temp.txt', 'jee2', FILE_APPEND);
         die('joo');
     }
 
     public static function postInstall(Event $event)
     {
+        file_put_contents('/tmp/temp.txt', 'jee3', FILE_APPEND);
         die('foo');
     }
 
     public static function postUpdate(Event $event)
     {
+        file_put_contents('/tmp/temp.txt', 'jee4', FILE_APPEND);
         die('bar');
     }
 }
