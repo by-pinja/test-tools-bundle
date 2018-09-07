6dd701848b992b8f589bcd66cb4f5ba711fe6e7f
tarlepp <tarmo.leppanen@protacon.com>
2018-09-07 12:47:15 +0300

    lets try package events

--- Util/Package.php
+++ Util/Package.php
@@ -8,7 +8,11 @@ class Package
 {
     public static function postPackageInstall(PackageEvent $event)
     {
-
         die('jeee');
+    }
+
+    public static function postUpdateInstall(PackageEvent $event)
+    {
+        die('joo');
     }
 }
